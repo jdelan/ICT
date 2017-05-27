b0f9dfe63e097b8bd0ede1edaeaa52fe9697012c
Jesse Delan <jesse.delan@users.noreply.github.com>
2017-05-26 19:32:56 -0500

    Trying to get Articles Index page to call terms

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,10 +15,9 @@
 
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :stories, :dependent => :destroy
+  has_many :stories, :class_name => "Company", :foreign_key => "company_id"
   has_many :references, :through => :stories, :source => :company
   validates :user_id, :presence => true
-validates :user_id, :uniqueness => { :scope => [:title, :url] }
 validates :url, :presence => true
 validates :title, :presence => true
 validates :date, :presence => true
